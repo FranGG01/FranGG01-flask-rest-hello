dac51aa5b001e75104112a7bb39bfbecaba40d2b
Francisco García <g.garciafran201@gmail.com>
2025-06-16 17:21:20 +0000

    Proyecto terminado

--- src/models.py
+++ src/models.py
@@ -6,14 +6,71 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username:Mapped[str] = mapped_column(String(50),unique=True, nullable=False)
+    firstname:Mapped[str] = mapped_column(String(50),nullable=False)
+    lastname:Mapped[str] = mapped_column(String(50),nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            'username':self.username,
+            'firstname':self.firstname,
+            'lastname':self.lastname
+        }
+
+class Post(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'))
+    
+
+    def serialize(self):
+        return{
+            'id':self.id,
+            'user_id':self.user_id
+        }
+    
+class Follower(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    user_from_id:Mapped[int] = mapped_column(db.ForeignKey('user.id'))
+    user_to_id:Mapped[int] = mapped_column(db.ForeignKey('user.id'))
+
+
+    def serialize(self):
+        return{
+            'id':self.id,
+            'user_from_id':self.user_from_id,
+            'user_to_id':self.user_to_id
+        }
+    
+class Media(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    type:Mapped[str] = mapped_column(String(20),nullable=False)
+    url:Mapped[str] = mapped_column(String(300),nullable=False)
+    post_id:Mapped[int] = mapped_column(db.ForeignKey('post.id'))
+
+
+    def serialize(self):
+        return{
+            'id':self.id,
+            'type':self.type,
+            'url':self.url,
+            'post_id':self.post_id
+        }
+    
+class Comment(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    comment_text:Mapped[str] = mapped_column(String(500),nullable=False)
+    author_id:Mapped[int] = mapped_column(db.ForeignKey('user.id'))
+    post_id:Mapped[int] = mapped_column(db.ForeignKey('post.id'))
+
+
+    def serialize(self):
+        return{
+            'id':self.id,
+            'comment_text':self.comment_text,
+            'author_id':self.author_id,
+            'post_id':self.post_id
         }
